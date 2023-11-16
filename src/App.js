4f31d7259b29113395ae07a0fd4002aa70329cb0
Mersy-Dev <anuoluwafalaju@gmail.com>
2023-11-16 01:25:16 +0100

    Done with the portfolio and Resume Section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Features from './components/Features/Features';
 import Header from './components/Head/Header'
 import Home from "./components/Hero/Home"
+import Portfolio from './components/Portfolio/Portfolio';
+import Resume from './components/Resume/Resume';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Header/>
       <Home />
       <Features />
+      <Portfolio />
+      <Resume />
 
     </>
   );
